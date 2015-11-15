e369b418d165fa6b3c3ac7fa0dfa38e85dc60fea
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-11-15 13:56:44 +0200

    Add solution buget 100p

---
+++ buget/buget_palcu_100p.cpp
@@ -0,0 +1,42 @@
+/***************************************************
+ * Alex Palcuie
+ * FMI No Stress 2015 - buget - 100 puncte O(N log N + N)
+ * alex [dot] palcuie [at] gmail [dot] com
+ * http://blog.palcu.ro/
+****************************************************/
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> budgets;
+
+int main() {
+    ifstream fin("buget.in");
+    ofstream fout("buget.out");
+    
+    int n, b, sum=0;
+    fin >> n >> b;
+    for (int i=0; i<n; i++) {
+        int x; fin >> x;
+        sum += x;
+        budgets.push_back(x);
+    }
+
+    // Am putea să avem plafonul 1 dacă sumele sunt prea mari
+    budgets.push_back(1);
+    sort(budgets.begin(), budgets.end(), greater<int>());
+
+    for (int i=0; i<budgets.size(); i++) {
+        int possible_budget = sum + budgets[i] * i;
+        // cerr << possible_budget << endl;
+        if (possible_budget <= b) {
+            fout << budgets[i] << endl;
+            break;
+        }
+        sum -= budgets[i];
+    }
+    
+    return 0;
+}
+
