7013452260df8a2cf9aabed73d16fa0a67196a97
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-11-20 19:27:30 +0200

    Solve the new problem

--- buget/buget_palcu_100p.cpp
+++ buget/buget_palcu_100p.cpp
@@ -5,40 +5,54 @@
  * http://blog.palcu.ro/
 ****************************************************/
 #include <algorithm>
+#include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-#define int64 long long 
+#define int64 long long
 
-vector<int> budgets;
+vector<int> budgets, partial_sums;
 
 int main() {
     ifstream fin("buget.in");
     ofstream fout("buget.out");
-    
-    int n; int64 b, sum=0;
+
+    int n; int64 b, max_x=-1;
     fin >> n >> b;
     for (int i=0; i<n; i++) {
         int x; fin >> x;
-        sum += x;
+        max_x = max_x > x ? max_x : x;
         budgets.push_back(x);
     }
 
-    // Am putea să avem plafonul 1 dacă sumele sunt prea mari
-    budgets.push_back(1);
-    sort(budgets.begin(), budgets.end(), greater<int>());
-
-    for (int i=0; i<budgets.size(); i++) {
-        int64 possible_budget = sum + budgets[i] * i;
-        // cerr << possible_budget << endl;
-        if (possible_budget <= b) {
-            fout << budgets[i] << endl;
-            break;
+    int left = 1, right = max_x, cap = -1;
+    while (left <= right) {
+        cap = right - (right-left) / 2;
+        cerr << "cap = " << cap << endl;
+        int64 sum = 0;
+        for (auto x : budgets) {
+            if (x <= cap) {
+                sum += x;
+            } else {
+                sum += cap;
+            }
         }
-        sum -= budgets[i];
+        cerr << "sum = " << sum << endl;
+
+        if (sum == b) {
+            right = cap;
+            break;
+        } else if (sum > b) {
+            right = cap - 1;
+        } else {
+            left = cap + 1;
+        }
     }
-    
+    fout << right << "\n";
+
     return 0;
 }
